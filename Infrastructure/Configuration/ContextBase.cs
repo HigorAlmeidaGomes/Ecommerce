f2b0fdd13ef95573a40f341aefbe7be060d1d0e9
Higor Ameida Gomes <higor_hag@hotmail.com>
2021-08-10 14:23:51 -0300

    Inclusão do projeto Ecommerce com a arquitetura DDD

---
+++ Infrastructure/Configuration/ContextBase.cs
@@ -0,0 +1,45 @@
+﻿using Entities.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Configuration
+{
+    public class ContextBase : IdentityDbContext<ApplicationUser>
+    {
+        public ContextBase(DbContextOptions<ContextBase> options) : base(options)
+        {
+        }
+
+        public DbSet<Produto> Produto { get; set; }
+        public DbSet<CompraUsuario> CompraUsuario { get; set; }
+        public DbSet<ApplicationUser> ApplicationUser { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                base.OnConfiguring(optionsBuilder.UseSqlServer(GetStringConectionConfig()));
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<ApplicationUser>().ToTable("AspNetUsers").HasKey(t => t.Id);
+
+            base.OnModelCreating(builder);
+        }
+
+
+        private string GetStringConectionConfig()
+        {
+            return "Data Source=PC-1571; Initial Catalog=Ecommerce; User Id=sa; Password=Senha 1234;";
+        }
+
+
+    }
+}
